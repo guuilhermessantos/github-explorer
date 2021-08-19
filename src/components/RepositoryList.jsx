03cb5b6119d8029a15c6fea3d2c4fe64d76f8707
Guilherme Santos <gsantos@action.local>
2021-08-18 22:10:18 -0300

    Conceitos importantes - Ignite ReactJS

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform',
+}
+
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+  return (
+    <section className="repository-list">
+      <h1> Lista de repositórios</h1>
+
+      <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
+      </ul>
+    </section>
+  )
+}
